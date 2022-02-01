fcdbb114a07bfe2e7d64a353c7f87a66f6119648
seetharamu gn <seetharamu@unotechsoft.com>
2022-02-02 02:42:28 +0530

    change to jar file

--- src/main/java/com/cymmetri/common/SwaggerConfig.java
+++ src/main/java/com/cymmetri/common/SwaggerConfig.java
@@ -1,15 +1,15 @@
 /*
- * Copyright (c) 2020 Unotech Software Pvt. Ltd.
+ * Copyright (c) 2020 wipeoutQ Pvt. Ltd.
  *
  * All Rights Reserved.
  *
- * The software contained on this media is written by  Unotech Software Pvt. Ltd. and
- * is proprietary to and embodies the confidential technology of Unotech Software.
+ * The software contained on this media is written by  wipeoutQ Pvt. Ltd. and
+ * is proprietary to and embodies the confidential technology of wipeoutQ.
  *
  * The possession or receipt of this information does not convey any right to disclose
  * its contents, reproduce it, or use, or license the use, for manufacture or sale,
  * the information or anything described therein. Any use, disclosure, or reproduction
- * without prior written permission of Unotech Software is strictly prohibited.
+ * without prior written permission of wipeoutQ is strictly prohibited.
  */
 package com.cymmetri.common;
 
